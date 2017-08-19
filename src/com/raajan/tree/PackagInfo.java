c442fd725de3294f656899439f87c2cfeb465334
rajanashish <rajan.ashishbabu@gmail.com>
2017-08-20 02:41:08 +0530

    Adding description

--- src/com/raajan/tree/PackagInfo.java
+++ src/com/raajan/tree/PackagInfo.java
@@ -3,8 +3,24 @@ package com.raajan.tree;
 /**
  * To Print all all ancestor of given node in binary tree
  *
- * @see {@link BstTree#getAllAncestors(Node, Node)
+ * {@link BstTree#initialiseTree() }<br>
  *
+ * {@link BstTree#getAllAncestors(Node, Node)} <br>
+ *
+ * {@link BstTree#successor(BstTree, int) }<br>
+ * 
+ * {@link BstTree#deleteNode(BstTree, int) }<br>
+ *
+ * {@link BstTree#getNode(BstTree, int) }<br>
+ *
+ * {@link BstTree#inorder(Node) }<br>
+ *
+ * {@link BstTree#levelOrderTraversal(BstTree) }<br>
+ *
+ * {@link BstTree#successor(BstTree, int) }<br>
+ *
+ * {@link BstTree#deleteNode(BstTree, int) }<br>
  */
 
-public class PackagInfo{}
+public class PackagInfo {
+}
