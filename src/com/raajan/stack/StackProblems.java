201428ce10ad476cd3f8bbea6dbf2c6a3a8cc1a4
rajanashish <rajan.ashishbabu@gmail.com>
2017-09-01 10:54:35 +0530

    Stack sorting

--- src/com/raajan/stack/StackProblems.java
+++ src/com/raajan/stack/StackProblems.java
@@ -1,5 +1,59 @@
 package com.raajan.stack;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class StackProblems {
+
+   public static void sortStackUsingRecursion(Stack<Integer> stack) {
+      if (stack.isEmpty())
+         return;
+      int top = stack.pop();
+      sortStackUsingRecursion(stack);
+      sortedInsert(stack, top);
+   }
+
+   private static void sortedInsert(Stack<Integer> stack, int element) {
+      if (stack.isEmpty() || element > stack.peek())
+         stack.push(element);
+      else {
+         int top = stack.pop();
+         sortedInsert(stack, element);
+         stack.push(top);
+      }
+   }
+
+
+   public static void main(String[] args) {
+      Stack<Integer> stack = new Stack<>();
+      int i = 0;
+
+      Random random = new Random();
+      while (i < 20) {
+         stack.push(random.nextInt() % 31);
+         i++;
+      }
+      System.out.println(stack.toString());
+      // sortStackUsingRecursion(stack);
+      Stack<Integer> tempStack = new Stack<>();
+      sortStackUsingTempStack(stack, tempStack);
+      System.out.println(tempStack.toString());
+
+   }
+
+   public static void sortStackUsingTempStack(Stack<Integer> stack, Stack<Integer> tempStack) {
+      if (!stack.isEmpty()) {
+         int top = stack.pop();
+         if (!tempStack.isEmpty() && top < tempStack.peek()) {
+            stack.push(tempStack.pop());
+            sortStackUsingTempStack(stack, tempStack);
+            tempStack.push(top);
+         } else {
+            tempStack.push(top);
+            sortStackUsingTempStack(stack, tempStack);
+         }
+      }
+      return;
+   }
 
 }
